21e27d1cd4d9345ce6a958d9ac473613ddd0bee6
Rafał Borowski <moderator9781@gmail.com>
2023-12-16 13:16:31 +0100

    Add icons to drawer

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,22 @@
 <template>
   <v-app id="inspire">
-    <v-navigation-drawer :width="231" v-model="drawer">
+    <v-navigation-drawer :width="231" v-model="drawer" >
       <v-list-item title="Vuetify Todo App" subtitle="Best Todo Ever"></v-list-item>
       <v-divider></v-divider>
-      <v-list-item :to="{ name: 'Home' }" title="Todo"></v-list-item>
-      <v-list-item :to="{ name: 'About' }" title="About"></v-list-item>
+      <v-list density="compact" nav> 
+        <v-list-item :to="{ name: 'Home' }" prepend-icon="mdi-format-list-checks" title="Todo"></v-list-item>
+        <v-list-item :to="{ name: 'About' }" prepend-icon="mdi-help-box" title="About"></v-list-item>
+      </v-list>
     </v-navigation-drawer>
 
     <v-app-bar>
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-
       <v-app-bar-title>{{ $route.name }}</v-app-bar-title>
     </v-app-bar>
 
     <v-main>
       <router-view />
-    </v-main>
+    </v-main>    
   </v-app>
 </template>
 
